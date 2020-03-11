1d7ac7a310a94ed190ad1f7e67fe54dea8f78bc4
bojan.miric <bojan.miric@omnitechit.eu>
2020-03-11 11:04:45 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router' // react-router v4/v5
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './configureStore'
+
+const store = configureStore(/* provide initial state if any */)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}> { /* place ConnectedRouter under Provider */ }
+      <> { /* your usual react-router v4/v5 routing */ }
+        <Switch>
+          <Route exact path="/" render={() => (<div>Match</div>)} />
+          <Route render={() => (<div>Miss</div>)} />
+        </Switch>
+      </>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('app')
+);
